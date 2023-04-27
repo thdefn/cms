f6bc79a232e63f8ccf86f14213da165b9567bc09
haden <80521474+thdefn@users.noreply.github.com>
2023-04-27 16:45:00 +0900

    feat : [001-EMAIL-Connect] mailgun에 대한 email 연동
    무료 계정으로 테스트 시 이메일 등록 반드시 필요

--- user-api/src/test/java/com/zerobase/cms/user/client/service/EmailSendServiceTest.java
+++ user-api/src/test/java/com/zerobase/cms/user/client/service/EmailSendServiceTest.java
@@ -1,22 +1,30 @@
 package com.zerobase.cms.user.client.service;
 
-import com.zerobase.cms.user.config.FeignConfig;
-import feign.Response;
+import com.zerobase.cms.user.client.MailgunClient;
+import com.zerobase.cms.user.client.mailgun.SendMailForm;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class EmailSendServiceTest {
-
     @Autowired
-    private EmailSendService emailSendService;
+    private MailgunClient mailgunClient;
 
     @Test
-    void sendMail() {
-        String response = emailSendService.sendMail();
+    void SuccessEmailTest() {
+        //given
+        SendMailForm form = SendMailForm.builder()
+                .from("thdefn@gmail.com")
+                .to("thdefn@gmail.com")
+                .subject("test email from zero base")
+                .text("my text")
+                .build();
+        //when
+        String result = mailgunClient.sendEmail(form);
+        //then
+        assertNotNull(result);
     }
 }
