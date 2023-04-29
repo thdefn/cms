53254ab2ab5bebf107717b6d7f22820efeada2f6
haden <80521474+thdefn@users.noreply.github.com>
2023-04-29 17:20:20 +0900

    feat : [008-INIT-order-api] order-api 초기 세팅

---
+++ order-api/src/main/java/com/zerobase/cms/order/OrderApiApplication.java
@@ -0,0 +1,22 @@
+package com.zerobase.cms.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@ServletComponentScan
+@EnableJpaRepositories(repositoryBaseClass = EnversRevisionRepositoryFactoryBean.class)
+@EnableJpaAuditing
+@SpringBootApplication
+@EnableFeignClients
+public class OrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApiApplication.class, args);
+	}
+
+}
